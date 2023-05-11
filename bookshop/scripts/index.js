0e6069231a73540cf512194cd190ca238f922b20
Paulin Majorovskaia <majorovpaul@gmail.com>
2023-05-11 17:01:49 +0300

    third commit

--- bookshop/scripts/index.js
+++ bookshop/scripts/index.js
@@ -7,46 +7,56 @@ architecture.addEventListener("click", submitForm);
 artAndFashion.addEventListener("click", submitForm);
 
 function cropDescription(description, size) {
-    if (description.length <= size) {
-      return description;
-    } else {
-      return description.substr(0, size) + "...";
-    }
+  if (description.length <= size) {
+    return description;
+  } else {
+    return description.substr(0, size) + "...";
+  }
 }
 
-
 function submitForm(e) {
-    e.preventDefault();
-    fetch(`${apiUrl}?q="subject:Architecture"&key=${apiKey}&printType=books&startIndex=0&maxResults=6&langRestrict=en`)
-        .then(data => data.json())
-        .then(data => {
-            console.log(data)
-            const bookList = document.querySelector('.books');
+  e.preventDefault();
+  fetch(
+    `${apiUrl}?q="subject:Architecture"&key=${apiKey}&printType=books&startIndex=0&maxResults=6&langRestrict=en`
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      console.log(data);
+      const bookList = document.querySelector(".books");
 
-            data.items.forEach(book => {
-                bookTemplate =
-                    `<div class="books-list">
+      data.items.forEach((book) => {
+        bookTemplate = `<div class="books-list">
                         <div class="books-list__cover" >
-                            <img src=${book.volumeInfo.imageLinks.thumbnail}></div>
+                            <img src=${
+                              book.volumeInfo.imageLinks.thumbnail
+                            }></div>
                             <div class="books-list__description">
-                                <p>${book.volumeInfo.authors}</p>
+                                <p class="author">${book.volumeInfo.authors}</p>
                                 <p class="title">${book.volumeInfo.title}</p>
-                                <p class="average-raiting">${book.volumeInfo.averageRating}</p>
-                                <p class="count-raiting">${book.volumeInfo.ratingsCount}</p>
-                                <div class="desc">${cropDescription(book.volumeInfo.description, 100)}</div>
-                                <p class="price">${book.saleInfo.retailPrice}</p>
+                                <div class="raiting-list">
+                                <span class="average-raiting">${
+                                  book.volumeInfo.averageRating
+                                }</span>
+                                <p class="count-raiting">${
+                                  book.volumeInfo.ratingsCount
+                                }</p></div>
+                                <div class="desc">${cropDescription(
+                                  book.volumeInfo.description,
+                                  100
+                                )}</div>
+                                <p class="price">${
+                                  book.saleInfo.retailPrice
+                                }</p>
                                 <div class="books-list__button">
                                     <button class="button-buy">buy now</button>
                                 </div>
                             </div>
                         </div>
-                    </div>`
+                    </div>`;
 
-                bookList.innerHTML += bookTemplate
-        })
-    })
+        bookList.innerHTML += bookTemplate;
+      });
+    });
 }
 
-
 document.addEventListener("DOMContentLoaded", submitForm);
-
